7f09c577326ba276e265e593b8b603c12e9c79da
Uraeus Millet <uraeusmillet@gmail.com>
2019-02-16 02:23:45 +0000

    (eloquent javascript) pass all sum tests

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -2,16 +2,47 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function range() {
-
+function range(num1, num2) {
+  //takes two numbers and returns an array of all number between two input numbers
+  //create ranger array
+  var array = [];
+  //if first number is larger than second number
+  if(num1 > num2) {
+    //loop backwards
+    for(let i = num1; i >= num2; i--) {
+      // push numbers into array
+      array.push(i);
+    }
+  } else if(num1 < num2) {
+    //loop forward
+    for(let i = num1; i <= num2; i++) {
+      //push numbers into array
+      array.push(i);
+    }
+  } else {
+    return array;
+  }
+  //return array
+  return array;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function sum() {
-
+function sum(array) {
+  //takes a range of numbers and adds them. returns sum
+  //create sum
+  var sum  = 0;
+  //only add if there are elements in array
+  if(array.length > 0) {
+    //loop array
+    for(let i = 0; i < array.length; i++) {
+      //reassign sum to add each number
+      sum += array[i];
+    }
+  }
+  return sum;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
