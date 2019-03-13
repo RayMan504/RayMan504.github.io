93d928fca64b63efec002fabec0a473afa8731f7
Uraeus Millet <uraeusmillet@gmail.com>
2019-03-13 15:48:15 +0000

    (eloquent javascript) dominant direction tests pass

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -41,9 +41,50 @@ function every(array, test) {
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
+//add helper function that identifies what script text is using by looping characters
+function characterScript(code) {
+  console.log('hey')
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+//add helper that counts characters in each script
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
 
 function dominantDirection(text) {
   console.log(text, 'chuhj')
+  //store result of countBy and filter countBy array by direction
+  let scripts = countBy(text, char => {
+    //reassign script to result of characterScript call.
+    let script = characterScript(char.codePointAt(0));
+    //return script objects with direction prop or assign none to scripts with no direction prop
+    return script ? script.direction : "none";
+  }).filter(({direction}) => direction != "none");
+  //use reduce to iterate scripst array and return script that has most direction count
+  return scripts.reduce((start, script) => {
+    if(script.count > start.count) {
+      start = script;
+    }
+    return start;
+  }, {count: 0}).name;
   
 }
 
